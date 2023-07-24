24c0f8c7c37d08750777773faa098a559e1d47d2
aima-a <140292382+aima-a@users.noreply.github.com>
2023-07-23 21:38:05 -0400

    Add files via upload

---
+++ GreaterThan.py
@@ -0,0 +1,11 @@
+#TODO: write a program to compare two numbers
+x = input("Enter first number") # Displays message to user & collects input
+y = input("Enter second number")
+x = int(x) # Converts user's input into an integer
+y = int(y)
+if x > y: # Instructions
+    print("The first number is greater than the second number")
+else:
+    print("The second number is greater than the first number")
+
+
